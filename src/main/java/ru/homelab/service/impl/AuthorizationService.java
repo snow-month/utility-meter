87ba36103c4f4ba0d5f4bbf8372c3758071af2aa
Petr <snow_day@rambler.ru>
2024-02-12 20:49:58 +0300

    отрефакторил AuthorizationService

--- src/main/java/ru/homelab/service/impl/AuthorizationService.java
+++ src/main/java/ru/homelab/service/impl/AuthorizationService.java
@@ -1,9 +1,7 @@
 package ru.homelab.service.impl;
 
-import ru.homelab.controller.UserController;
 import ru.homelab.entity.MessageAudit;
 import ru.homelab.entity.User;
-import ru.homelab.exception.NoUserException;
 import ru.homelab.service.AuditService;
 // todo service?
 
@@ -19,17 +17,14 @@ public class AuthorizationService {
      */
 // todo getCurrentUser?
     public static final ThreadLocal<User> CURRENT_USER = new ThreadLocal<>();
-    private final UserController userController;
     private final AuditService auditService;
 
     /**
      * Instantiates a new Authorization service.
      *
-     * @param userController the user controller
-     * @param auditService   the audit service
+     * @param auditService the audit service
      */
-    public AuthorizationService(UserController userController, AuditService auditService) {
-        this.userController = userController;
+    public AuthorizationService(AuditService auditService) {
         this.auditService = auditService;
     }
 
@@ -40,25 +35,25 @@ public class AuthorizationService {
      * @param password the password
      * @return the boolean
      */
-    public boolean authorization(String login, String password) {
-        try {
-            User userDb = userController.getUserByLogin(login);
-            if (userDb.getPassword().equals(password)) {
-                User user = new User(
-                        userDb.getId(),
-                        userDb.getLogin(),
-                        null,
-                        userDb.getRole()
-                );
-                CURRENT_USER.set(user);
-                auditService.save(MessageAudit.AUTHORIZATION);
-                return true;
-            }
-        } catch (NoUserException e) {
-            return false;
-        }
-        return false;
-    }
+//    public boolean authorization(String login, String password) {
+//        try {
+//            User userDb = userController.getUserByLogin(login);
+//            if (userDb.getPassword().equals(password)) {
+//                User user = new User(
+//                        userDb.getId(),
+//                        userDb.getLogin(),
+//                        null,
+//                        userDb.getRole()
+//                );
+//                CURRENT_USER.set(user);
+//                auditService.save(MessageAudit.AUTHORIZATION);
+//                return true;
+//            }
+//        } catch (NoUserException e) {
+//            return false;
+//        }
+//        return false;
+//    }
 
     /**
      * Метод для logout.
