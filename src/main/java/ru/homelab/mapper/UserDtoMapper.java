a8302c5598be83b544828241e20f774c0c10c899
Petr <snow_day@rambler.ru>
2024-02-16 12:41:19 +0300

    добавил игнор id в UserDtoMapper

--- src/main/java/ru/homelab/mapper/UserDtoMapper.java
+++ src/main/java/ru/homelab/mapper/UserDtoMapper.java
@@ -1,6 +1,7 @@
 package ru.homelab.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import ru.homelab.dto.UserDto;
 import ru.homelab.entity.User;
@@ -9,5 +10,6 @@ import ru.homelab.entity.User;
 public interface UserDtoMapper {
     UserDtoMapper INSTANCE = Mappers.getMapper(UserDtoMapper.class);
 
+    @Mapping(target = "id", ignore = true)
     User userDtoToUser(UserDto userDto);
 }
